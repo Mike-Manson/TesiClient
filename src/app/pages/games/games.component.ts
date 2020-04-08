1de5089493b0eebefa08a5b23a043e1d936547fd
Marco Mansolillo <m.mansolillo93@gmail.com>
2020-04-08 12:33:05 +0200

    aggiunta componente form

--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TableDto } from 'src/app/components/table/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-games',
@@ -36,9 +37,13 @@ export class GamesComponent implements OnInit {
     ]
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navTo() {
+    this.router.navigate['game-form'];
   }
 
 }
